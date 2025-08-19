06529f82b68a71134a18220c731ff288fafda5f2
arihantkadam396-crypto <arihantkadam396@gmail.com>
2025-08-19 19:36:04 +0530

    Create sample_anime.dart

---
+++ lib/models/lib/models/lib/data/sample_anime.dart
@@ -0,0 +1,40 @@
+import '../models/anime.dart';
+
+const sampleAnime = <Anime>[
+  Anime(
+    id: 'naruto-shippuden',
+    title: 'Naruto Shippuden',
+    imageUrl:
+        'https://upload.wikimedia.org/wikipedia/en/7/7e/Naruto_Shippuden_key_visual.jpg',
+    description:
+        'Naruto Uzumaki continues his quest to become Hokage while facing the Akatsuki and saving his friend Sasuke.',
+    genre: 'Action, Adventure',
+  ),
+  Anime(
+    id: 'aot',
+    title: 'Attack on Titan',
+    imageUrl:
+        'https://upload.wikimedia.org/wikipedia/en/5/5a/Attack_on_Titan_cover.jpg',
+    description:
+        'Humanity battles against Titans while uncovering dark secrets beyond the walls.',
+    genre: 'Action, Drama',
+  ),
+  Anime(
+    id: 'demonslayer',
+    title: 'Demon Slayer',
+    imageUrl:
+        'https://upload.wikimedia.org/wikipedia/en/8/86/Kimetsu_no_Yaiba_Volume_1.png',
+    description:
+        'Tanjiro Kamado becomes a Demon Slayer to avenge his family and cure his sister Nezuko.',
+    genre: 'Action, Fantasy',
+  ),
+  Anime(
+    id: 'jjk',
+    title: 'Jujutsu Kaisen',
+    imageUrl:
+        'https://upload.wikimedia.org/wikipedia/en/5/5f/Jujutsu_kaisen.jpg',
+    description:
+        'Yuji Itadori joins Tokyo Jujutsu High to fight curses after ingesting Sukuna\'s finger.',
+    genre: 'Action, Supernatural',
+  ),
+];
