72082d8c131ee218ed544e2d11cbda10d492ec78
arihantkadam396-crypto <arihantkadam396@gmail.com>
2025-08-19 19:34:20 +0530

    Create anime.dart

---
+++ lib/models/anime.dart
@@ -0,0 +1,15 @@
+class Anime {
+  final String id;
+  final String title;
+  final String imageUrl;
+  final String description;
+  final String genre;
+
+  const Anime({
+    required this.id,
+    required this.title,
+    required this.imageUrl,
+    required this.description,
+    required this.genre,
+  });
+}
